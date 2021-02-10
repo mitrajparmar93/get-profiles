f0f5c865e19250ef17c802dec117bc96ba829444
Parmar, Mitraj (CORP) <Mitraj.Parmar@adp.com>
2021-02-09 23:21:05 -0800

    initial commit

---
+++ getprofiles.py
@@ -0,0 +1,57 @@
+from os import read
+import requests
+import json
+import re
+
+# disable ssl warnings
+requests.packages.urllib3.disable_warnings()
+
+
+#f5ip = rawdata("Enter the F5 IP Address: ")
+f5ip = '192.168.1.5'
+f5auth = 'YWRtaW46YWRtaW4='
+
+# Get the device version
+versionuri = '/mgmt/tm/cli/version'
+vurl = "https://"+f5ip+versionuri+"?expandSubcollections=true"
+
+payload = {}
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic '+f5auth+"'",
+}
+
+vresponse = requests.request(
+    "GET", vurl, headers=headers, data=payload, verify=False)
+f5version = vresponse.json()[
+    u'entries'][u'https://localhost/mgmt/tm/cli/version/0'][u'nestedStats'][u'entries'][u'active'][u'description']
+
+vernum = (str(f5version).split(".", 1)[0])
+
+# Get the profile data
+
+with open('/home/parmarm0/scripts/f5profiles/profilescope', 'r') as f, \
+        open('/home/parmarm0/scripts/f5profiles/temp', 'w') as w:
+    w.write('[\n')
+    for line in f.readlines():
+        prftype, prfname = line.split()
+        w.write('{\n')
+        w.write((f'\"profileType\": \"{prftype}\" , \"profileName\": \"{prfname}\",'))
+        w.write('\n\"profileData\":\n')
+
+        prfuri = '/mgmt/tm/ltm/profile/'+prftype+"/"+prfname
+        prfurl = "https://"+f5ip+prfuri+"?expandSubcollections=true"
+
+        prfresponse = requests.request(
+            "GET", prfurl, headers=headers, data=payload, verify=False)
+        json.dump(prfresponse.json(), w)
+        w.write('\n},')
+    w.write('\n]')
+
+    regex = r"(\,\n\])"
+    subst = "\\n]"
+
+with open('/home/parmarm0/scripts/f5profiles/temp', 'r') as regexdata, \
+        open('/home/parmarm0/scripts/f5profiles/v'+vernum+'/profiledata.json', 'w') as regexwrite:
+    result = re.sub(regex, subst, regexdata.read(), flags=re.M)
+    regexwrite.write(result)
